daaf9ba03b4ab8632ab346494a6469a4eb368075
Julian.Schaefer <julian.schaefer@gdata.de>
2018-03-07 13:41:24 +0100

    Added missing function to replace German additional characters

--- src/main/java/Rot13Encoder.java
+++ src/main/java/Rot13Encoder.java
@@ -2,6 +2,12 @@ public class Rot13Encoder {
 
     private static final int OFFSET = 13;
     private static final int UPPERCASE_Z_IN_ASCII_TABLE = 90;
+    private static final String UMLAUT_Ä = "Ä";
+    private static final String AE = "AE";
+    private static final String UMLAUT_Ö = "Ö";
+    private static final String OE = "OE";
+    private static final String UMLAUT_Ü = "Ü";
+    private static final String UE = "UE";
 
 
     public static void main(String[] args){
@@ -10,6 +16,7 @@ public class Rot13Encoder {
     }
     private static void encode(String input, int offset){
         input = input.toUpperCase();
+        input = replaceGermanAdditionalChars(input);
         StringBuilder sb = new StringBuilder();
         for (int i =0; i< input.length();i++){
             int position = input.charAt(i);
@@ -24,6 +31,13 @@ public class Rot13Encoder {
             sb.append(Character.toString((char) newPosition));
         }
         System.out.println(sb.toString().toUpperCase());
+    }
+
+    private static String replaceGermanAdditionalChars(String input) {
+        input = input.replaceAll(UMLAUT_Ä, AE);
+        input = input.replaceAll(UMLAUT_Ö, OE);
+        input = input.replaceAll(UMLAUT_Ü, UE);
+        return input;
     }
 
     private static boolean inAsciiAlphabet(int position) {

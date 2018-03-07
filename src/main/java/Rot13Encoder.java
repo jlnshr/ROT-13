1cdbce226b4313c2ef9d8150a038465fd8459178
Julian.Schaefer <julian.schaefer@gdata.de>
2018-03-07 13:11:04 +0100

    Initial commit with workin class

---
+++ src/main/java/Rot13Encoder.java
@@ -0,0 +1,36 @@
+public class Rot13Encoder {
+
+    private static final int OFFSET = 13;
+    private static final int UPPERCASE_Z_IN_ASCII_TABLE = 90;
+
+
+    public static void main(String[] args){
+        encode("Hello, World");
+        encode("Lorem ipsum!0987!");
+    }
+    private static void encode(String input){
+        input = input.toUpperCase();
+        StringBuilder sb = new StringBuilder();
+        for (int i =0; i< input.length();i++){
+            int position = input.charAt(i);
+            if(inAsciiAlphabet(position)){
+                sb.append(input.charAt(i));
+                continue;
+            }
+            int newPosition = (int) input.charAt(i) + OFFSET;
+            if(newASCIIGreaterThan90(newPosition)){
+                newPosition -=26;
+            }
+            sb.append(Character.toString((char) newPosition));
+        }
+        System.out.println(sb.toString().toUpperCase());
+    }
+
+    private static boolean inAsciiAlphabet(int position) {
+        return position <65 | position > 90;
+    }
+
+    private static boolean newASCIIGreaterThan90(int newPosition) {
+        return newPosition > UPPERCASE_Z_IN_ASCII_TABLE;
+    }
+}
